fa0f277d31564928dbbf2f27e14cbbc73ac1220c
janpfeiffer7-stack <jan.pfeiffer7@gmail.com>
2026-09-13 13:46:49 +0200

    Create config.js

---
+++ mathe-gym/src/config.js
@@ -0,0 +1,5 @@
+export const SUPABASE_URL = 'https://aazyebrhjzsfnmbozttw.supabase.co';
+
+export const SUPABASE_ANON_KEY = 'sb_publishable_C8HDfw1yGwiYakgZs1Z-cg_nAnhxW4b';
+
+export const SCHUELER_DOMAIN = 'mathegym.local';
